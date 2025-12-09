4fa745a641e0b52a11095d32300a8ee04af40fe4
primesourcellp <recruit@primesourcellp.com>
2025-12-09 13:07:23 +0530

    Update Payroll.jsx

--- frontend/src/pages/Payroll.jsx
+++ frontend/src/pages/Payroll.jsx
@@ -1226,6 +1226,7 @@ const Payroll = () => {
                       </div>
                     </div>
                     
+                    
                     <div>
                       <label className="block text-sm font-semibold text-gray-700 mb-2">Bonus</label>
                       <div className="relative">
